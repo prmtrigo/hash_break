297a3daf0aa7819bd01f56a111c270892eeb42bb
prmtrigo <paulormtrigo@gmail.com>
2025-04-18 14:00:50 -0300

    testando a quebra de senha

---
+++ gerador.py
@@ -0,0 +1,14 @@
+import hashlib
+
+def gerar_hash (senha):
+    return hashlib.sha256(senha.encode()).hexdigest()
+
+senha = input("Digite sua senha: ")
+
+hash_senha = gerar_hash(senha)
+print(f"\nSua senha: '{senha}'")
+print(f"\nHash da senha: {hash_senha}")
+
+with open ("hash_senha.txt", "w") as arquivo:
+    arquivo.write(hash_senha)
+    print(f"\nHash da senha foi salvo em hash_senha.txt")
